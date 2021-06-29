d2dbf6297606269ca75d1c74a1700a097bf4ec37
Pina <connect@pinasystems.com>
2021-06-29 06:59:20 -0700

    Update UrlHelper.cs

--- OAWA.Data/Helpers/UrlHelper.cs
+++ OAWA.Data/Helpers/UrlHelper.cs
@@ -2,6 +2,6 @@ namespace OAWA.Data.Helpers
 {
     public class UrlHelper
     {
-        public static string baseUrl ="http://localhost:5000/StaticFiles/";
+        public static string baseUrl ="http://www.oawa.co.in/StaticFiles/";
     }
 }
